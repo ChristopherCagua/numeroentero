820899f7d1daf0d52384ce17b32b6026513835f2
ChristopherCagua <caguachristopher@gmail.com>
2021-06-21 09:22:35 -0500

    Verificando el numero entero

---
+++ cmake-build-debug/CMakeFiles/numeroentero.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numeroentero.dir/main.cpp.obj"
+  "libnumeroentero.dll.a"
+  "numeroentero.exe"
+  "numeroentero.exe.manifest"
+  "numeroentero.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numeroentero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
